7e6ba05f22d2d412c2a1fd02f45abd5fae94e780
owolabi-victor <vicowolabi22@gmail.com>
2025-08-03 01:02:22 +0100

    update2

--- src/components/update.tsx
+++ src/components/update.tsx
@@ -1,32 +1,32 @@
-import type { Person } from "../App";
-import  {Button}  from "../App";
-import PhoneBookService from '../services/phoneBook'
+// import type { Person } from "../App";
+// import  {Button}  from "../App";
+// import PhoneBookService from '../services/phoneBook'
 
 
-type UpdateProps = {
-    persons: Person[];
-    id: number;
-}
+// type UpdateProps = {
+//     persons: Person[];
+//     id: number;
+// }
 
-const handleUpdate = (id: number, updatedEntry: Person) => {
-  PhoneBookService.update(id, updatedEntry)
-    .then((returnedPerson) => {
-      setPersons((prev) =>
-        prev.map((p) => (p.id === id ? returnedPerson : p))
-      );
-    })
-    .catch((error) => {
-      console.error('Error updating person:', error);
-    });
-};
+// const handleUpdate = (id: number, updatedEntry: Person) => {
+//   PhoneBookService.update(id, updatedEntry)
+//     .then((returnedPerson) => {
+//       setPersons((prev) =>
+//         prev.map((p) => (p.id === id ? returnedPerson : p))
+//       );
+//     })
+//     .catch((error) => {
+//       console.error('Error updating person:', error);
+//     });
+// };
 
 
-const handleUpdatePrompt = ({persons, id}: UpdateProps) => {
-  const person = persons.find((p) => p.id === id);
-  const newNumber = prompt(`Update number for ${person?.name}:`, person?.number);
+// const handleUpdatePrompt = ({persons, id}: UpdateProps) => {
+//   const person = persons.find((p) => p.id === id);
+//   const newNumber = prompt(`Update number for ${person?.name}:`, person?.number);
 
-  if (newNumber && person) {
-    const updated = { ...person, number: newNumber };
-    handleUpdate(id, updated);
-  }
-};
+//   if (newNumber && person) {
+//     const updated = { ...person, number: newNumber };
+//     handleUpdate(id, updated);
+//   }
+// };
